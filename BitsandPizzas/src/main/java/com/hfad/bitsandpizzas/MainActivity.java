e1421a7a13566bced2dc5161ad301cf5d2969c5f
Rainer Telesko <rainer.telesko@gmail.com>
2022-02-27 18:44:32 +0100

    Update versions (Android + Gradle)

--- BitsandPizzas/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
+++ BitsandPizzas/src/main/java/com/hfad/bitsandpizzas/MainActivity.java
@@ -1,13 +1,14 @@
 package com.hfad.bitsandpizzas;
 
 import android.content.Intent;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.ShareActionProvider;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 public class MainActivity extends AppCompatActivity {
     private ShareActionProvider shareActionProvider;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
--- BitsandPizzas/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
+++ BitsandPizzas/src/main/java/com/hfad/bitsandpizzas/OrderActivity.java
@@ -1,9 +1,10 @@
 package com.hfad.bitsandpizzas;
 
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class OrderActivity extends AppCompatActivity {
 
